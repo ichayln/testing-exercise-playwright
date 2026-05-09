3ef1ca3c115a2eee32ea25348e16ff6f85684fc2
ichayln <yulianicha29@gmail.com>
2026-05-09 20:19:45 +0700

    indentation - sign up

--- signup.spec.js
+++ signup.spec.js
@@ -1,33 +1,35 @@
 import { test, expect } from '@playwright/test';
 
-//function random username
-function generateUserName(){
-    return 'user_' + Date.now()
+// Function random username
+function generateUserName() {
+  return 'user_' + Date.now();
 }
 
-//function random username
-function generatePassword(){
-    return 'pass_' + Date.now()
+// Function random password
+function generatePassword() {
+  return 'pass_' + Date.now();
 }
 
-test ('Test SIGNUP', async({ page }) => {
- await page.goto('https://www.demoblaze.com/')
+test('Test SIGNUP', async ({ page }) => {
+  await page.goto('https://www.demoblaze.com/');
 
- //Generate random username
- const username = generateUserName()
+  // Generate random username
+  const username = generateUserName();
 
- //Generate random password
- const password = generatePassword()
+  // Generate random password
+  const password = generatePassword();
 
-//Click signup button
- await page.getByRole('link', { name: 'Sign up' }).click()
- await page.getByRole('textbox', { name: 'Username:' }).fill(username)
- await page.getByRole('textbox', { name: 'Password:' }).fill(password)
+  // Click signup button
+  await page.getByRole('link', { name: 'Sign up' }).click();
+  await page.getByRole('textbox', { name: 'Username:' }).fill(username);
+  await page.getByRole('textbox', { name: 'Password:' }).fill(password);
 
-//Successfully sign-up
- await page.getByRole('button', { name: 'Sign up' }).click()
+  // Successfully sign-up
+  await page.getByRole('button', { name: 'Sign up' }).click();
 
-//Screenshoot page sign-up
-await page.screenshot({ path: 'screenshoot/01-signup.png', fullPage: true });
-
+  // Screenshot page sign-up
+  await page.screenshot({
+    path: 'screenshoot/01-signup.png',
+    fullPage: true,
+  });
 });
