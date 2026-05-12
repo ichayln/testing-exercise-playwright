2bb85ef27b052e49839d38fc0dc50d2877c68b17
ichayln <yulianicha29@gmail.com>
2026-05-12 19:51:09 +0700

    indentation

--- checkout.spec.js
+++ checkout.spec.js
@@ -1,48 +1,58 @@
 import { test, expect } from '@playwright/test';
 
-test ('Test Checkout', async({ page }) => {
- await page.goto('https://www.demoblaze.com/')
+test('Test Checkout', async ({ page }) => {
+  await page.goto('https://www.demoblaze.com/');
 
-//Choose product
- await page.getByRole('link', { name: 'Nokia lumia' }).click()
+  // Choose product
+  await page.getByRole('link', { name: 'Nokia lumia' }).click();
 
-//Verify detail
- await expect(page.getByRole('heading', { name: 'Nokia lumia' })).toBeVisible()
+  // Verify detail
+  await expect(
+    page.getByRole('heading', { name: 'Nokia lumia' })
+  ).toBeVisible();
 
-//Click add to cart
- await page.getByRole('link', { name: 'Add to cart' }).click()
+  // Wait for alert dialog
+  const dialogPromise = page.waitForEvent('dialog');
 
-//Jeda
-  await page.waitForTimeout(2000);
+  // Click add to cart
+  await page.getByRole('link', { name: 'Add to cart' }).click();
 
-//Click cart
+  // Verify dialog
+  const dialog = await dialogPromise;
+  expect(dialog.message()).toContain('Product added');
+  await dialog.accept();
+
+  // Click cart
   await page.locator('#cartur').click();
 
-//Tunggu cart load
+  // Wait cart load
   await page.waitForSelector('#tbodyid');
 
-//Verify cart
-  const cartText = await page.locator('#tbodyid').textContent();
-  expect(cartText).toContain('Nokia lumia');
+  // Verify cart
+  await expect(page.locator('#tbodyid')).toContainText('Nokia lumia');
 
-//Click place oder
-  await page.getByRole('button', { name: 'Place Order' }).click()
+  // Click place order
+  await page.getByRole('button', { name: 'Place Order' }).click();
 
-//Isi form 
-  await page.fill('#name', 'Budi')
-  await page.fill('#country', 'Indonesia')
-  await page.fill('#city', 'Jakarta')
-  await page.fill('#card', '6789')
-  await page.fill('#month', '07')
-  await page.fill('#year', '2029')
+  // Isi form
+  await page.fill('#name', 'Budi');
+  await page.fill('#country', 'Indonesia');
+  await page.fill('#city', 'Jakarta');
+  await page.fill('#card', '6789');
+  await page.fill('#month', '07');
+  await page.fill('#year', '2029');
 
-//Click purchase
-  await page.getByRole('button', { name: 'Purchase' }).click()
+  // Click purchase
+  await page.getByRole('button', { name: 'Purchase' }).click();
 
-//Verify success message
-  await expect(page.locator('.sweet-alert')).toContainText('Thank you for your purchase')
+  // Verify success message
+  await expect(page.locator('.sweet-alert')).toContainText(
+    'Thank you for your purchase'
+  );
 
-//Screenshoot success message
-await page.screenshot({ path: 'screenshoot/08-checkout.png', fullPage: true });
-
+  // Screenshot success message
+  await page.screenshot({
+    path: 'screenshoot/08-checkout.png',
+    fullPage: true,
+  });
 });
